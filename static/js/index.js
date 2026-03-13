10eb03795534c6217a6535caf1cf7053dad4bc9b
GOWTHAM A <146941646+gowthamkce@users.noreply.github.com>
2026-03-13 21:08:57 +0530

    fix production api url

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,4 @@
-const API_BASE =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
-    ? "http://127.0.0.1:5000"
-    : "https://portfolio-cms-flask.onrender.com";
-
+const API_BASE = "https://portfolio-cms-flask.onrender.com";
 const API = API_BASE + "/api";
 
 const LEVELS = { Beginner:28, Intermediate:58, Advanced:80, Expert:96 };
